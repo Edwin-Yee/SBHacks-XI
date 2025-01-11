1962474863fe3dd6631a3c594b90339bb4881d86
Wizdro975 <wizdro579@gmail.com>
2025-01-11 11:26:53 -0800

    Added Google authentication

---
+++ voting-app/app/home/redirect.ts
@@ -0,0 +1,15 @@
+// app/redirect.ts
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Redirect() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/login');
+    }, [router]);
+
+    return null;
+}
---
+++ voting-app/app/login/page.tsx
@@ -0,0 +1,22 @@
+// app/login/page.tsx
+'use client';
+
+import React, { useState } from 'react';
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId = '376855101707-6rvk4fqkhq301cd6ar1340pagpd7cea1.apps.googleusercontent.com';
+
+const Login: React.FC = () => {
+    return (
+        <GoogleOAuthProvider clientId={clientId}>
+            <div className="login-container">
+                <h1>Login</h1>
+                <GoogleLogin onSuccess={(credentialResponse) => {
+                    console.log(credentialResponse);
+                }} onError={() => console.log("Login Failed")} />
+            </div>
+        </GoogleOAuthProvider>
+    );
+};
+
+export default Login;
